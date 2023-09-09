90d8681ba4a056c3bc1471c14f5b16e8cd4dc567
Kacper <kacper.tylec1999@gmail.com>
2023-09-09 21:05:54 +0200

    MongoDB options setup and repository setup

---
+++ Services/Apartments/Apartments.API/Configurations/MongoSettingsSetup.cs
@@ -0,0 +1,20 @@
+﻿using Apartments.Infrastructure.Db;
+using Microsoft.Extensions.Options;
+
+namespace Apartments.API.Configurations
+{
+    public class MongoSettingsSetup: IConfigureOptions<MongoSettings>
+    {
+        private const string Section = "MongoDB";
+        private readonly IConfiguration _configuration;
+
+        public MongoSettingsSetup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void Configure(MongoSettings options)
+        {
+            _configuration.GetSection(Section).Bind(options);
+        }
+    }
+}
--- Services/Apartments/Apartments.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Apartments.Application
-{
-    public class Class1
-    {
-
-    }
-}
