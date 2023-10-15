44199c963b06fb5bf5942921fbf93d4c4be27c60
Kacper <kacper.tylec1999@gmail.com>
2023-10-15 12:09:54 +0200

    Dto

--- Services/Apartments/Apartments.Application/Dtos/ApartmentDto.cs
+++ Services/Apartments/Apartments.Application/Dtos/ApartmentDto.cs
@@ -8,21 +8,13 @@ namespace Apartments.Application.Dtos
 {
     public class ApartmentDto
     {
-        public string? Id { get; set; }
-        public LocalizationDto Localization { get; set; }
-        public SurfaceInfoDto SurfaceInfo { get; set; }
-        public string LandlordId { get; set; }
-        public string Telephone { get; set; }
-    }
-    public class LocalizationDto
-    {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
-    public class SurfaceInfoDto
-    {
-        public int RoomsNumber { get; private set; }
-        public int Area { get; private set; }
+        public string? apartmentId { get; set; }
+        public string LandlordId { get;  set; }
+        public double Latitude { get;  set; }
+        public double Longitude { get;  set; }
+        public int RoomsNumber { get; set; }
+        public int Area { get;  set; }
+        public string Telephone { get;   set; }
     }
 
 }
