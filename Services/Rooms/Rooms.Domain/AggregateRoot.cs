11c30cfffba48270665c381be06fbb38e19f1e14
Kacper <kacper.tylec1999@gmail.com>
2023-10-17 22:42:23 +0200

    Aggregate Root

---
+++ Services/Rooms/Rooms.Domain/AggregateRoot.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Rooms.Domain
+{
+    public abstract class AggregateRoot
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId Id { get; set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime LastModifiedDate { get; private set; }
+
+        public void SetCreationDate() { CreationDate = DateTime.Now; }
+        public void SetLastModifiedDate() { LastModifiedDate = DateTime.Now; }
+    }
+}
--- Services/Rooms/Rooms.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Rooms.Domain
-{
-    public class Class1
-    {
-
-    }
-}
