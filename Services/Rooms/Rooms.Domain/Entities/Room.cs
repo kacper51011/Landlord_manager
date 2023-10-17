774ff893fa2bec9e82b4d3d99ab39603978e9b8c
Kacper <kacper.tylec1999@gmail.com>
2023-10-17 23:21:59 +0200

    room rich domain model

--- Services/Rooms/Rooms.Domain/Entities/Room.cs
+++ Services/Rooms/Rooms.Domain/Entities/Room.cs
@@ -8,12 +8,43 @@ namespace Rooms.Domain.Entities
 {
     public class Room: AggregateRoot
     {
-        public string RoomId { get; set; }
-        public string Name { get; set; }
-        public int Surface { get; set; }
-        public string AnglesCoordinates { get; set; }
-        public int MaxTenantsNumber { get; set; }
-        public int CurrentTenantsNumber { get; set; }
-        public int MonthlyRent { get; set; }
+        private Room() { }
+        public string RoomId { get; private set; }
+        public string ApartmentId { get; private set; }
+        public string Name { get; private set; }
+        public int Surface { get; private set; }
+        public string AnglesCoordinates { get; private set; }
+        public int MaxTenantsNumber { get; private set; }
+        public int CurrentTenantsNumber { get; private set; }
+        public int MonthlyRent { get; private set; }
+
+        public static Room CreateRoom(string apartmentId, string name, int surface, string anglesCoordinates, int maxTenantsNumber, int currentTenantsNumber, int monthlyRent)
+        {
+            Room room = new Room()
+            {
+                RoomId = Guid.NewGuid().ToString(),
+                ApartmentId = apartmentId,
+                Name = name,
+                Surface = surface,
+                AnglesCoordinates = anglesCoordinates,
+                MaxTenantsNumber = maxTenantsNumber,
+                CurrentTenantsNumber = currentTenantsNumber,
+                MonthlyRent = monthlyRent
+            };
+            room.SetCreationDate();
+            room.SetLastModifiedDate();
+            return room;
+        }
+        public Room UpdateRoom(string name, int surface, string anglesCoordinates, int maxTenantsNumber, int currentTenantsNumber, int monthlyRent)
+        {
+            Name = name;
+            Surface = surface;
+            AnglesCoordinates = anglesCoordinates;
+            MaxTenantsNumber = maxTenantsNumber;
+            CurrentTenantsNumber = currentTenantsNumber;
+            MonthlyRent = monthlyRent;
+            SetLastModifiedDate();
+            return this;
+        }
     }
 }
