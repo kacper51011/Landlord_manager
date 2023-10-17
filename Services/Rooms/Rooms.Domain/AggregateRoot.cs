1c11b0fb9a3c63b82b1422f02bd46c1aad6cdcc6
Kacper <kacper.tylec1999@gmail.com>
2023-10-17 23:00:54 +0200

    Room entity initial properties

--- Services/Rooms/Rooms.Domain/AggregateRoot.cs
+++ Services/Rooms/Rooms.Domain/AggregateRoot.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Services/Rooms/Rooms.Domain/Entities/Room.cs
+++ Services/Rooms/Rooms.Domain/Entities/Room.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Rooms.Domain.Entities
 {
-    internal class Room
+    public class Room: AggregateRoot
     {
+        public string RoomId { get; set; }
+        public string Name { get; set; }
+        public int Surface { get; set; }
+        public string AnglesCoordinates { get; set; }
+        public int MaxTenantsNumber { get; set; }
+        public int CurrentTenantsNumber { get; set; }
+        public int MonthlyRent { get; set; }
     }
 }
