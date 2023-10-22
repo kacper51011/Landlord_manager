78cc33f65edff0c754b0edfcb212c581c439e5d5
Kacper <kacper.tylec1999@gmail.com>
2023-10-22 20:28:55 +0200

    DeleteAllRooms init

---
+++ Services/Rooms/Rooms.Application/Commands/DeleteAllRooms/DeleteAllRoomsCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rooms.Application.Commands.DeleteAllRooms
+{
+    internal class DeleteAllRoomsCommandHandler
+    {
+    }
+}
