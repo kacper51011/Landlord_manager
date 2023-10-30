a01043d4b2716b1462d7596535ba5a5c93aec709
Kacper <kacper.tylec1999@gmail.com>
2023-10-30 14:47:33 +0100

    Docker updates, Rooms commands

--- Services/Rooms/Rooms.Application/Commands/DeleteRoom/DeleteRoomCommand.cs
+++ Services/Rooms/Rooms.Application/Commands/DeleteRoom/DeleteRoomCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Rooms.Application.Commands.DeleteRoom
 {
-    internal class DeleteRoomCommand
+    public record DeleteRoomCommand(string landlordId, string roomId) : IRequest
     {
     }
 }
--- Services/Rooms/Rooms.Application/Commands/DeleteRoom/DeleteRoomCommandHandler.cs
+++ Services/Rooms/Rooms.Application/Commands/DeleteRoom/DeleteRoomCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MediatR;
+using Rooms.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace Rooms.Application.Commands.DeleteRoom
 {
-    internal class DeleteRoomCommandHandler
+    public class DeleteRoomCommandHandler : IRequestHandler<DeleteRoomCommand>
     {
+        private readonly IRoomsRepository _roomRepository;
+        public DeleteRoomCommandHandler(IRoomsRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+        public async Task Handle(DeleteRoomCommand request, CancellationToken cancellationToken)
+        {
+            var room = await _roomRepository.GetRoomByIdAndLandlordId(request.landlordId, request.roomId);
+            if (room == null)
+            {
+                return;
+            }
+            else
+            {
+                await _roomRepository.DeleteRoom(request.roomId);
+            }
+
+        }
     }
 }
