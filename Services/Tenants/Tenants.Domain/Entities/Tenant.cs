fb25fc479268b87cfc3cb412cae838b54e0bbf3b
Kacper <kacper.tylec1999@gmail.com>
2023-11-01 14:29:44 +0100

    Tenant Entity

---
+++ Services/Tenants/Tenants.Domain/Entities/Tenant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tenants.Domain.Entities
+{
+    public class Tenant : AggregateRoot
+    {
+        public string RoomId { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public bool IsStudying { get; private set; }
+        public bool isWorking { get; private set; }
+        public string Email { get; private set; }
+        public int Rent { get; private set; }
+        public DateOnly ContractStart { get; private set; }
+        public DateOnly ContractEnd { get; private set;}
+
+    }
+}
