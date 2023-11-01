84e28ee838a520477f7bc592b448b542ee397f21
Kacper <kacper.tylec1999@gmail.com>
2023-11-01 16:24:00 +0100

    Tenants repository interface

---
+++ Services/Tenants/Tenants.Domain/Interfaces/ITenantsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tenants.Domain.Entities;
+
+namespace Tenants.Domain.Interfaces
+{
+    public interface ITenantsRepository
+    {
+        public Task<List<Tenant>> GetTenantsByRoomId(string roomId);
+        public Task CreateOrUpdateTenant(Tenant tenant);
+        public Task<Tenant> GetTenantByIdAndRoomId(string tenantId, string roomId);
+        public Task DeleteTenant(string tenantId);
+
+        public Task DeleteAllTenantsInRoom(string RoomId);
+        public Task<Tenant> GetTenantById(string roomId);
+    }
+}
