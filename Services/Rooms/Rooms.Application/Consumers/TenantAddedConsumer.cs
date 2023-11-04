dcaf5b4d299bc08308081fd995c2ac95d31dca71
Kacper <kacper.tylec1999@gmail.com>
2023-11-04 16:37:29 +0100

    TenantAddedConsumer

--- Services/Rooms/Rooms.Application/Consumers/TenantAddedConsumer.cs
+++ Services/Rooms/Rooms.Application/Consumers/TenantAddedConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts.TenantsServiceEvents;
 using MassTransit;
+using Rooms.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,33 @@ namespace Rooms.Application.Consumers
 {
     public class TenantAddedConsumer : IConsumer<TenantAddedEvent>
     {
+        private readonly IRoomsRepository _roomsRepository;
+
+        public TenantAddedConsumer(IRoomsRepository roomsRepository)
+        {
+            _roomsRepository = roomsRepository;
+            
+        }
 
         public async Task Consume(ConsumeContext<TenantAddedEvent> context)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var room = await _roomsRepository.GetRoomById(context.Message.roomId);
+                if (room == null)
+                {
+                    return;
+                }
+                room.IncrementTenantNumber();
+                await _roomsRepository.CreateOrUpdateRoom(room);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
         }
     }
 }
