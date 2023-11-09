dc97350e00fd8e0e2d97a6b70b40a101c524c78a
Kacper <kacper.tylec1999@gmail.com>
2023-11-09 19:40:00 +0100

    GetTenantsByRoomIdQuery

--- Services/Tenants/Tenants.Application/Queries/GetTenants/GetTenantsByRoomIdQueryHandler.cs
+++ Services/Tenants/Tenants.Application/Queries/GetTenants/GetTenantsByRoomIdQueryHandler.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Mapster;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tenants.Application.Dtos;
+using Tenants.Domain.Interfaces;
 
 namespace Tenants.Application.Queries.GetTenants
 {
-    internal class GetTenantsByRoomIdQueryHandler
+    public class GetTenantsByRoomIdQueryHandler : IRequestHandler<GetTenantsByRoomIdQuery, List<TenantDto>>
     {
+        private readonly ITenantsRepository _tenantRepository;
+        private readonly ILogger<GetTenantsByRoomIdQueryHandler> _logger;
+        public GetTenantsByRoomIdQueryHandler(ITenantsRepository tenantRepository, ILogger<GetTenantsByRoomIdQueryHandler> logger)
+        {
+            
+            _tenantRepository = tenantRepository;
+            _logger = logger;
+        }
+        public async Task<List<TenantDto>> Handle(GetTenantsByRoomIdQuery request, CancellationToken cancellationToken)
+        {
+			try
+			{
+                var tenants = await _tenantRepository.GetTenantsByRoomId(request.roomId);
+                List<TenantDto> response = new List<TenantDto>();
+                if (tenants == null)
+                {
+                    _logger.LogDebug($"Can`t find tenants for room with ID {request.roomId}");
+                    return response;
+                }
+                foreach (var tenant in tenants )
+                {
+                    
+                    TenantDto tenantDto = tenant.Adapt<TenantDto>();
+                    response.Add(tenantDto);
+                }
+                return response;
+
+
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+        }
     }
 }
