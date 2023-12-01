c1d7bb8e5aeb7df1a2e89122696ca0db891f7cd6
Kacper <kacper.tylec1999@gmail.com>
2023-12-01 22:14:52 +0100

    Initialized ApartmentsStatistics Entity

--- Services/Statistics/Statistics.Domain/Entities/ApartmentsStatistics.cs
+++ Services/Statistics/Statistics.Domain/Entities/ApartmentsStatistics.cs
@@ -8,8 +8,20 @@ namespace Statistics.Domain.Entities
 {
     public class ApartmentsStatistics: AggregateRoot
     {
+        private ApartmentsStatistics(DateTime start, DateTime end)
+        {
+            ApartmentsStatisticsId = Guid.NewGuid().ToString();
+            StatisticsStart = start;
+            StatisticsEnd = end;
+            ApartmentsCreated = 0;
+            IsApartmentsCreatedProcessed = false;
+            ApartmentsUpdated = 0;
+            IsApartmentsUpdatedProcessed = false;
+            MostApartmentsOwnedByUser = 0;
+            IsMostApartmentsOwnedByUserProcessed = false;
+
+    }
         public string ApartmentsStatisticsId { get; private set; }
-        //public string StatisticsScope { get; private set; }
         public DateTime StatisticsStart { get; private set; }
         public DateTime StatisticsEnd { get; private set; }
         public int ApartmentsCreated { get; private set; }
@@ -18,5 +30,12 @@ namespace Statistics.Domain.Entities
         public bool IsApartmentsUpdatedProcessed { get; private set; }
         public int MostApartmentsOwnedByUser { get; private set; }
         public bool IsMostApartmentsOwnedByUserProcessed { get; private set; }
+
+    public ApartmentsStatistics Create(DateTime statisticsStart, DateTime statisticsEnd)
+    {
+        return new ApartmentsStatistics(statisticsStart, statisticsEnd);
     }
+    }
+
+
 }
