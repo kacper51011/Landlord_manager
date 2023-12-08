203c70ef4b8d1480a813dce90bc47219b6c35ee4
Kacper <kacper.tylec1999@gmail.com>
2023-12-08 19:36:48 +0100

    Statistics Value Objects

---
+++ Services/Statistics/Statistics.Domain/ValueObjects/Hour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Domain.ValueObjects
+{
+    public class Hour
+    {
+        public int Value { get; }
+
+        public Hour(int value)
+        {
+            Validate(value);
+            Value = value;
+        }
+
+        private void Validate(int value)
+        {
+            if (value < 0 || value > 23)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Hour must be between 0 and 23.");
+            }
+        }
+    }
+}
