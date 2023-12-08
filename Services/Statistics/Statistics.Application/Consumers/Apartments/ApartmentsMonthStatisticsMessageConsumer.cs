190da8d8787d305f97ac1f79123fff3709f1e57d
Kacper <kacper.tylec1999@gmail.com>
2023-12-09 00:17:25 +0100

    ApartmentsMonthStatisticsMessageConsumer

--- Services/Statistics/Statistics.Application/Consumers/Apartments/ApartmentsMonthStatisticsMessageConsumer.cs
+++ Services/Statistics/Statistics.Application/Consumers/Apartments/ApartmentsMonthStatisticsMessageConsumer.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Contracts.StatisticsMessages;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Statistics.Domain.Entities;
+using Statistics.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,36 @@ using System.Threading.Tasks;
 
 namespace Statistics.Application.Consumers.Apartments
 {
-    internal class ApartmentsMonthStatisticsMessageConsumer
+    public class ApartmentsMonthStatisticsMessageConsumer: IConsumer<StatisticMonthMessage>
     {
+        private readonly ILogger<ApartmentsMonthStatisticsMessageConsumer> _logger;
+        private readonly IApartmentsStatisticsRepository _apartmentsStatisticsRepository;
+        public ApartmentsMonthStatisticsMessageConsumer(ILogger<ApartmentsMonthStatisticsMessageConsumer> logger, IApartmentsStatisticsRepository apartmentsStatisticsRepository)
+        {
+            _logger = logger;
+            _apartmentsStatisticsRepository = apartmentsStatisticsRepository;
+
+        }
+        public async Task Consume(ConsumeContext<StatisticMonthMessage> context)
+        {
+            try
+            {
+                var response = await _apartmentsStatisticsRepository.GetApartmentMonthStatistics(context.Message.Year, context.Message.Month);
+                if (response != null)
+                {
+                    _logger.LogInformation($"Month apartment statistic for date {response.StatisticsStart} was already created");
+                    return;
+                }
+                ApartmentsStatistics apartmentStatistic = ApartmentsStatistics.CreateAsMonthStatisticsInformations(context.Message.Year, context.Message.Month);
+                await _apartmentsStatisticsRepository.CreateOrUpdateApartmentStatistics(apartmentStatistic);
+                _logger.LogInformation($"Month apartment statistic created with Id {apartmentStatistic.ApartmentsStatisticsId}");
+                return;
+            }
+            catch (Exception)
+            {
+                _logger.LogError($"Something went wrong in ApartmentsMonthStatisticsMessageConsumer");
+                throw;
+            }
+        }
     }
 }
