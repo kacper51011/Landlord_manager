0d281100e328b46d81fd813b9baf4d431b368c11
Kacper <kacper.tylec1999@gmail.com>
2023-12-09 00:10:23 +0100

    ApartmentsHourStatisticsMessageConsumer

---
+++ Services/Statistics/Statistics.Application/Consumers/Apartments/ApartmentsHourStatisticsMessageConsumer.cs
@@ -0,0 +1,46 @@
+﻿using Contracts.StatisticsMessages;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Statistics.Domain.Entities;
+using Statistics.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Application.Consumers.Apartments
+{
+    public class ApartmentsHourStatisticsMessageConsumer: IConsumer<StatisticHourMessage>
+    {
+        private readonly ILogger<ApartmentsHourStatisticsMessageConsumer> _logger;
+        private readonly IApartmentsStatisticsRepository _apartmentsStatisticsRepository;
+        public ApartmentsHourStatisticsMessageConsumer(ILogger<ApartmentsHourStatisticsMessageConsumer> logger, IApartmentsStatisticsRepository apartmentsStatisticsRepository)
+        {
+            _logger = logger;
+            _apartmentsStatisticsRepository = apartmentsStatisticsRepository;
+
+        }
+        public async Task Consume(ConsumeContext<StatisticHourMessage> context)
+        {
+            try
+            {
+                var response = await _apartmentsStatisticsRepository.GetApartmentHourStatistics(context.Message.Year, context.Message.Month, context.Message.Day, context.Message.Hour);
+                if (response != null)
+                {
+                    _logger.LogInformation($"Hour apartment statistic for date {response.StatisticsStart} was already created");
+                    return;
+                }
+                ApartmentsStatistics apartmentStatistic = ApartmentsStatistics.CreateAsHourStatisticsInformations(context.Message.Year, context.Message.Month, context.Message.Day, context.Message.Hour);
+                await _apartmentsStatisticsRepository.CreateOrUpdateApartmentStatistics(apartmentStatistic);
+                _logger.LogInformation($"Hour apartment statistic created with Id {apartmentStatistic.ApartmentsStatisticsId}");
+                return;
+            }
+            catch (Exception)
+            {
+                _logger.LogError($"Something went wrong in ApartmentsHourStatisticsMessageConsumer");
+                throw;
+            }
+        }
+    }
+}
