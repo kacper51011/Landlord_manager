452b9977097a1c1f782bb5b8847264ab8ce53364
Kacper <kacper.tylec1999@gmail.com>
2023-12-09 10:01:30 +0100

    RoomStatistics Entity

--- Services/Statistics/Statistics.Domain/Entities/RoomsStatistics.cs
+++ Services/Statistics/Statistics.Domain/Entities/RoomsStatistics.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Statistics.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +7,96 @@ using System.Threading.Tasks;
 
 namespace Statistics.Domain.Entities
 {
-    public class RoomsHourStatistics: AggregateRoot
+    public class RoomsStatistics: AggregateRoot
     {
-        public string RoomsHourStatisticsId { get; private set; }
-        //public string StatisticsScope { get; private set; }
+        private RoomsStatistics()
+        {
+            RoomsStatisticsId = Guid.NewGuid().ToString();
+            RoomsCreated = 0;
+            RoomsUpdated = 0;
+            BiggestCreatedRoomSize = 0;
+            MostRoomsInApartment = 0;
+            IsFullyProcessed = false;
+        }
+        public string RoomsStatisticsId { get; private set; }
+        public Year Year { get; private set; }
+        public Month? Month { get; private set; }
+        public Day? Day { get; private set; }
+        public Hour? Hour  { get; private set; }
         public DateTime StatisticsStart { get; private set; }
         public DateTime StatisticsEnd { get; private set; }
-
         public int RoomsCreated { get; private set; }
         public int RoomsUpdated { get; private set; }
-        public int BiggestRoom { get; private set; }
+        public int BiggestCreatedRoomSize { get; private set; }
         public int MostRoomsInApartment { get; private set; }
+        public bool IsFullyProcessed { get; private set; }
+        public string Scope { get; private set; }
+
+
+        public static RoomsStatistics CreateAsHourStatisticsInformations(int year, int month, int day, int hour)
+        {
+            return new RoomsStatistics()
+            {
+                Year = new Year(year),
+                Month = new Month(month),
+                Day = new Day(day),
+                Hour = new Hour(hour),
+                Scope = "Hour",
+                StatisticsStart = new DateTime(year, month, day, hour, 1, 1),
+                StatisticsEnd = new DateTime(year, month, day, hour + 1, 1, 1)
+            };
+
+        }
+        public static RoomsStatistics CreateAsDayStatisticsInformations(int year, int month, int day)
+        {
+            return new RoomsStatistics()
+            {
+                Year = new Year(year),
+                Month = new Month(month),
+                Day = new Day(day),
+                Hour = null,
+                Scope = "Day",
+                StatisticsStart = new DateTime(year, month, day),
+                StatisticsEnd = new DateTime(year, month, day + 1)
+            };
+        }
+
+        public static RoomsStatistics CreateAsMonthStatisticsInformations(int year, int month)
+        {
+            return new RoomsStatistics()
+            {
+                Year = new Year(year),
+                Month = new Month(month),
+                Day = null,
+                Hour = null,
+                Scope = "Month",
+                StatisticsStart = new DateTime(year, month, 1),
+                StatisticsEnd = new DateTime(year, month + 1, 1)
+            };
+        }
+
+        public static RoomsStatistics CreateAsYearStatisticsInformations(int year)
+        {
+            return new RoomsStatistics()
+            {
+                Year = new Year(year),
+                Month = null,
+                Day = null,
+                Hour = null,
+                Scope = "Year",
+                StatisticsStart = new DateTime(year, 1, 1),
+                StatisticsEnd = new DateTime(year + 1, 1, 1)
+            };
+
+        }
+
+        public void SetStatistics(int roomsCreated, int roomsUpdated, int biggestCreatedRoomSize, int mostRoomsInApartment)
+        {
+            RoomsCreated = roomsCreated;
+            RoomsUpdated = roomsUpdated;
+            BiggestCreatedRoomSize = biggestCreatedRoomSize;
+            MostRoomsInApartment = mostRoomsInApartment;
+        }
     }
+
 }
