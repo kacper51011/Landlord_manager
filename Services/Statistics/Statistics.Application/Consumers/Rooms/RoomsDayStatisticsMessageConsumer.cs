0c2b9586fd3d21897b617b4b1202394a0e043d8a
Kacper <kacper.tylec1999@gmail.com>
2023-12-09 10:28:00 +0100

    RoomsDayStatisticsMessageConsumer

--- Services/Statistics/Statistics.Application/Consumers/Rooms/RoomsDayStatisticsMessageConsumer.cs
+++ Services/Statistics/Statistics.Application/Consumers/Rooms/RoomsDayStatisticsMessageConsumer.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Contracts.StatisticsMessages;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Statistics.Application.Consumers.Rooms;
+using Statistics.Domain.Entities;
+using Statistics.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,36 @@ using System.Threading.Tasks;
 
 namespace Statistics.Application.Consumers.Rooms
 {
-    internal class RoomsDayStatisticsMessageConsumer
+    public class RoomsDayStatisticsMessageConsumer: IConsumer<StatisticDayMessage>
     {
+        private readonly ILogger<RoomsDayStatisticsMessageConsumer> _logger;
+        private readonly IRoomsStatisticsRepository _roomsStatisticsRepository;
+        public RoomsDayStatisticsMessageConsumer(ILogger<RoomsDayStatisticsMessageConsumer> logger, IRoomsStatisticsRepository roomsStatisticsRepository)
+        {
+            _logger = logger;
+            _roomsStatisticsRepository = roomsStatisticsRepository;
+
+        }
+        public async Task Consume(ConsumeContext<StatisticDayMessage> context)
+        {
+            try
+            {
+                var response = await _roomsStatisticsRepository.GetRoomsDayStatistics(context.Message.Year, context.Message.Month, context.Message.Day);
+                if (response != null)
+                {
+                    _logger.LogInformation($"Day room statistic for date {response.StatisticsStart} was already created");
+                    return;
+                }
+                RoomsStatistics roomStatistic = RoomsStatistics.CreateAsDayStatisticsInformations(context.Message.Year, context.Message.Month, context.Message.Day);
+                await _roomsStatisticsRepository.CreateOrUpdateRoomsStatistics(roomStatistic);
+                _logger.LogInformation($"Day room statistic created with Id {roomStatistic.RoomsStatisticsId}");
+                return;
+            }
+            catch (Exception)
+            {
+                _logger.LogError($"Something went wrong in RoomsDayStatisticsMessageConsumer");
+                throw;
+            }
+        }
     }
 }
