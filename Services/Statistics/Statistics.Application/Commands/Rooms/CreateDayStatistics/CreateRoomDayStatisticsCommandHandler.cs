f194070464c301725708191ce72a81d114fe31ab
Kacper <kacper.tylec1999@gmail.com>
2023-12-13 19:08:54 +0100

    Room Commands

---
+++ Services/Statistics/Statistics.Application/Commands/Rooms/CreateDayStatistics/CreateRoomDayStatisticsCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Statistics.Domain.Entities;
+using Statistics.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Application.Commands.Rooms.CreateDayStatistics
+{
+    public class CreateRoomDayStatisticsCommandHandler : IRequestHandler<CreateRoomDayStatisticsCommand>
+    {
+        private readonly IRoomsStatisticsRepository _repository;
+        private readonly ILogger<CreateRoomDayStatisticsCommandHandler> _logger;
+        public CreateRoomDayStatisticsCommandHandler(IRoomsStatisticsRepository repository, ILogger<CreateRoomDayStatisticsCommandHandler> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+
+        }
+        public async Task Handle(CreateRoomDayStatisticsCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var data = await _repository.GetRoomsDayStatistics(request.RequestDto.Year, request.RequestDto.Month, request.RequestDto.Day);
+                if (data != null)
+                {
+                    throw new DuplicateNameException("Statistic already exists");
+                }
+                var statistic = RoomsStatistics.CreateAsDayStatisticsInformations(request.RequestDto.Year, request.RequestDto.Month, request.RequestDto.Day);
+                await _repository.CreateOrUpdateRoomsStatistics(statistic);
+                return;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ Services/Statistics/Statistics.Application/Commands/Rooms/CreateHourStatistics/CreaterRoomHourStatisticsCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Statistics.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Application.Commands.Rooms.CreateHourStatistics
+{
+
+}
