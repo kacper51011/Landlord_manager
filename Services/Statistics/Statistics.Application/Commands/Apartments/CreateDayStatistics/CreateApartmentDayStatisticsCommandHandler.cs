151f0fff914fb1c81c4fe8a622372677f5e4afef
Kacper <kacper.tylec1999@gmail.com>
2023-12-13 15:44:23 +0100

    CreateApartmentDayStatisticsCommand

---
+++ Services/Statistics/Statistics.Application/Commands/Apartments/CreateDayStatistics/CreateApartmentDayStatisticsCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Statistics.Domain.Entities;
+using Statistics.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Application.Commands.Apartments.CreateDayStatistics
+{
+    public class CreateApartmentDayStatisticsCommandHandler : IRequestHandler<CreateApartmentDayStatisticsCommand>
+    {
+        private readonly IApartmentsStatisticsRepository _repository;
+        private readonly ILogger<CreateApartmentDayStatisticsCommandHandler> _logger;
+        public CreateApartmentDayStatisticsCommandHandler(IApartmentsStatisticsRepository repository, ILogger<CreateApartmentDayStatisticsCommandHandler> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+
+        }
+        public async Task Handle(CreateApartmentDayStatisticsCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var data = await _repository.GetApartmentDayStatistics(request.RequestDto.Year, request.RequestDto.Month, request.RequestDto.Day);
+                if (data != null)
+                {
+                    throw new DuplicateNameException("Statistic already exists");
+                }
+                var statistic = ApartmentsStatistics.CreateAsDayStatisticsInformations(request.RequestDto.Year, request.RequestDto.Month, request.RequestDto.Day);
+                await _repository.CreateOrUpdateApartmentStatistics(statistic);
+                return;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
