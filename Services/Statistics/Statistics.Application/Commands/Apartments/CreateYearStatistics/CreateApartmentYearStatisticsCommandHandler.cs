314939b76f7f8aca7f9293de9b1094784cc96610
Kacper <kacper.tylec1999@gmail.com>
2023-12-13 16:00:32 +0100

    CreateApartmentYearStatisticsCommand

--- Services/Statistics/Statistics.Application/Commands/Apartments/CreateYearStatistics/CreateApartmentYearStatisticsCommandHandler.cs
+++ Services/Statistics/Statistics.Application/Commands/Apartments/CreateYearStatistics/CreateApartmentYearStatisticsCommandHandler.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Statistics.Application.Commands.Apartments.CreateYearStatistics;
+using Statistics.Domain.Entities;
+using Statistics.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Statistics.Application.Commands.Apartments.CreateYearStatistics
 {
-    internal class CreateApartmentYearStatisticsCommandHandler
+    public class CreateApartmentYearStatisticsCommandHandler : IRequestHandler<CreateApartmentYearStatisticsCommand>
     {
+        private readonly IApartmentsStatisticsRepository _repository;
+        private readonly ILogger<CreateApartmentYearStatisticsCommandHandler> _logger;
+        public CreateApartmentYearStatisticsCommandHandler(IApartmentsStatisticsRepository repository, ILogger<CreateApartmentYearStatisticsCommandHandler> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+
+        }
+        public async Task Handle(CreateApartmentYearStatisticsCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var data = await _repository.GetApartmentYearStatistics(request.RequestDto.Year);
+                if (data != null)
+                {
+                    throw new DuplicateNameException("Statistic already exists");
+                }
+                var statistic = ApartmentsStatistics.CreateAsYearStatisticsInformations(request.RequestDto.Year);
+                return;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
     }
 }
