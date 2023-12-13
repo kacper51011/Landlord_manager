5a90a4a1ff2ceaa440aa6a4d39263b1165664778
Kacper <kacper.tylec1999@gmail.com>
2023-12-13 15:44:37 +0100

    CreateApartmentHourStatisticCommand Handler

---
+++ Services/Statistics/Statistics.Application/Commands/Apartments/CreateHourStatistics/CreateApartmentHourStatisticsCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Statistics.Domain.Entities;
+using Statistics.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Application.Commands.Apartments.CreateHourStatistics
+{
+    public class CreateApartmentHourStatisticsCommandHandler : IRequestHandler<CreateApartmentHourStatisticsCommand>
+    {
+        private readonly IApartmentsStatisticsRepository _repository;
+        private readonly ILogger<CreateApartmentHourStatisticsCommandHandler> _logger;
+        public CreateApartmentHourStatisticsCommandHandler(IApartmentsStatisticsRepository repository, ILogger<CreateApartmentHourStatisticsCommandHandler> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+            
+        }
+        public async Task Handle(CreateApartmentHourStatisticsCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var data = await _repository.GetApartmentHourStatistics(request.RequestDto.Year, request.RequestDto.Month, request.RequestDto.Day, request.RequestDto.Hour);
+                if (data != null)
+                {
+                        throw new DuplicateNameException("Statistic already exists");
+                }
+                var statistic = ApartmentsStatistics.CreateAsHourStatisticsInformations(request.RequestDto.Year, request.RequestDto.Month, request.RequestDto.Day, request.RequestDto.Hour);
+                return;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
