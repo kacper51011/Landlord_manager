6723fa7debb7bce4da8bfa7fbc3bb5d1e24f4d88
Kacper <kacper.tylec1999@gmail.com>
2023-12-14 22:30:23 +0100

    CreateRoomYearStatisticsCommandHandler

--- Services/Statistics/Statistics.Application/Commands/Rooms/CreateYearStatistics/CreateRoomYearStatisticsCommandHandler.cs
+++ Services/Statistics/Statistics.Application/Commands/Rooms/CreateYearStatistics/CreateRoomYearStatisticsCommandHandler.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Statistics.Application.Commands.Rooms.CreateYearStatistics;
+using Statistics.Domain.Entities;
+using Statistics.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Statistics.Application.Commands.Rooms.CreateYearStatistics
 {
-    internal class CreateRoomYearStatisticsCommandHandler
+    public class CreateRoomYearStatisticsCommandHandler : IRequestHandler<CreateRoomYearStatisticsCommand>
     {
+        private readonly IRoomsStatisticsRepository _repository;
+        private readonly ILogger<CreateRoomYearStatisticsCommandHandler> _logger;
+        public CreateRoomYearStatisticsCommandHandler(IRoomsStatisticsRepository repository, ILogger<CreateRoomYearStatisticsCommandHandler> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+
+        }
+        public async Task Handle(CreateRoomYearStatisticsCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var data = await _repository.GetRoomsYearStatistics(request.RequestDto.Year);
+                if (data != null)
+                {
+                    throw new DuplicateNameException("Statistic already exists");
+
+                }
+                var statistic = RoomsStatistics.CreateAsYearStatisticsInformations(request.RequestDto.Year);
+                await _repository.CreateOrUpdateRoomsStatistics(statistic);
+                return;
+            }
+            catch (DuplicateNameException ex)
+            {
+                _logger.LogWarning(400, ex, ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogWarning(500, ex, "CreateRoomYearStatisticsCommandHandler failed");
+            }
+
+        }
     }
 }
