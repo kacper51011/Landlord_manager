8c251be637561c86761c77519fc57c4c238386c3
Kacper <kacper.tylec1999@gmail.com>
2023-12-25 13:41:33 +0100

    Tenants swagger documentation

--- Services/Tenants/Tenants.Api/Controllers/TenantsController.cs
+++ Services/Tenants/Tenants.Api/Controllers/TenantsController.cs
@@ -1,7 +1,5 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using Tenants.Application.Commands.CreateOrUpdateTenant;
 using Tenants.Application.Commands.DeleteTenant;
 using Tenants.Application.Dtos;
@@ -9,73 +7,105 @@ using Tenants.Application.Queries.GetTenants;
 
 namespace Tenants.Api.Controllers
 {
- 
-        [Route("api/[controller]")]
-        [ApiController]
-        public class TenantsController : ControllerBase
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenantsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public TenantsController(IMediator mediator)
         {
-            private readonly IMediator _mediator;
-            public TenantsController(IMediator mediator)
+            _mediator = mediator;
+        }
+
+
+
+        /// <summary>
+        /// Gets List of tenants for specified roomId
+        /// </summary>
+        /// <param name="roomId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<List<TenantDto>>> GetTenants(string roomId)
+        {
+            try
             {
-                _mediator = mediator;
+                var query = new GetTenantsByRoomIdQuery(roomId);
+                var response = await _mediator.Send(query);
+                return response;
             }
-
-            [HttpGet]
-            public async Task<IActionResult> GetTenants(string roomId)
+            catch (FileNotFoundException ex)
             {
-                try
-                {
-                    var query = new GetTenantsByRoomIdQuery(roomId);
-                    var response = await _mediator.Send(query);
-                    return Ok(response);
-                }
-                catch (Exception)
-                {
 
-                    throw;
-                }
+                return StatusCode(404, ex.Message);
             }
-
-            [HttpPost]
-            public async Task<IActionResult> CreateOrUpdateTenant([FromBody] TenantDto dto)
+            catch (Exception ex)
             {
-                try
-                {
-                    var command = new CreateOrUpdateTenantCommand(dto);
-                    await _mediator.Send(command);
 
-
-                    return Ok();
-
-
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex);
-                    return StatusCode(500);
-                }
-            }
-            [HttpDelete]
-            public async Task<IActionResult> DeleteTenant(string roomId, string tenantId)
-            {
-                try
-                {
-                    var command = new DeleteTenantCommand(roomId, tenantId);
-                    var isSuccess = await _mediator.Send(command);
-
-                    if (!isSuccess)
-                    {
-                        return BadRequest();
-                    }
-                    return Ok();
-                }
-                catch (Exception ex)
-                {
-
-                    Debug.WriteLine(ex);
-                    return StatusCode(500);
-                }
+                return StatusCode(500, ex.Message);
             }
         }
-    
+
+        /// <summary>
+        /// Creates or updates tenant, depends on including tenantId in body
+        /// </summary>
+        /// <param name="tenantDto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateOrUpdateTenant([FromBody] TenantDto tenantDto)
+        {
+            try
+            {
+                var command = new CreateOrUpdateTenantCommand(tenantDto);
+                await _mediator.Send(command);
+
+
+                return Ok();
+
+
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes tenant with specified tenantId in room with specified roomId
+        /// </summary>
+        /// <param name="tenantId"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> DeleteTenant(string tenantId)
+        {
+            try
+            {
+                var command = new DeleteTenantCommand(tenantId);
+                await _mediator.Send(command);
+                return Ok();
+            }
+            catch (FileNotFoundException ex)
+            {
+
+                return StatusCode(404, ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+
 }
