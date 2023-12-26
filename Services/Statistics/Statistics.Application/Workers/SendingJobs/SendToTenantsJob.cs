62ff4e35973026ea5285e6760160d98241411cad
Kacper <kacper.tylec1999@gmail.com>
2023-12-26 14:02:43 +0100

    SendToTenantsJob

--- Services/Statistics/Statistics.Application/Workers/SendingJobs/SendToTenantsJob.cs
+++ Services/Statistics/Statistics.Application/Workers/SendingJobs/SendToTenantsJob.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Contracts.StatisticsMessages.Tenants;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using Statistics.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,40 @@ using System.Threading.Tasks;
 
 namespace Statistics.Application.Workers.SendingJobs
 {
-    internal class SendToTenantsJob
+    public class SendToTenantsJob: IJob
     {
+        private readonly IPublishEndpoint _publisher;
+        private readonly ITenantsStatisticsRepository _repository;
+        private readonly ILogger<SendToTenantsJob> _logger;
+        public SendToTenantsJob(ITenantsStatisticsRepository repository, ILogger<SendToTenantsJob> logger, IPublishEndpoint publisher)
+        {
+            _repository = repository;
+            _logger = logger;
+            _publisher = publisher;
+            
+
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                var notSentStatistic = await _repository.GetNotSentTenantStatistics();
+                if (notSentStatistic == null)
+                {
+                    _logger.LogInformation("No Tenant statistic to send");
+                    return;
+                }
+                await _publisher.Publish(new ManuallyCreatedTenantStatisticsMessage { Hour = notSentStatistic.Hour.Value, Day = notSentStatistic.Day.Value, Month = notSentStatistic.Month.Value, Year = notSentStatistic.Year.Value });
+                notSentStatistic.SetIsSent(true);
+                await _repository.CreateOrUpdateTenantsStatistics(notSentStatistic);
+                _logger.LogInformation($"Sent ManuallyCreatedTenantStatisticsMessage for statistic with Id {notSentStatistic.TenantsStatisticsId}");
+
+            }
+            catch (Exception)
+            {
+                _logger.LogWarning($"Something went wrong in ManuallyCreatedTenantStatisticsMessage");
+                throw;
+            }
+        }
     }
 }
