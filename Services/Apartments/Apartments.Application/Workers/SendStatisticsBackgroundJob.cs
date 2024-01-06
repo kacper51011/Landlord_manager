25898735e1993085b7680a140fd91d6e1f671f0f
Kacper <kacper.tylec1999@gmail.com>
2024-01-06 15:39:01 +0100

    Background job for sending statistics from apartment service

--- Services/Apartments/Apartments.Application/Workers/SendStatisticsBackgroundJob.cs
+++ Services/Apartments/Apartments.Application/Workers/SendStatisticsBackgroundJob.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Apartments.Domain.Interfaces;
+using Contracts.StatisticsMessages.Apartments;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Quartz;
 
 namespace Apartments.Application.Workers
 {
-    public class SendStatisticsBackgroundJob
+    public class SendStatisticsBackgroundJob : IJob
     {
+        private readonly IApartmentsStatisticsRepository _apartmentsStatisticsRepository;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<SendStatisticsBackgroundJob> _logger;
+
+        public SendStatisticsBackgroundJob(IApartmentsStatisticsRepository apartmentsStatisticsRepository, IPublishEndpoint publishEndpoint, ILogger<SendStatisticsBackgroundJob> logger)
+        {
+            _apartmentsStatisticsRepository = apartmentsStatisticsRepository;
+            _publishEndpoint = publishEndpoint;
+            _logger = logger;
+
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                //atomically finds and updates the statistic
+                var notSendStatistic = await _apartmentsStatisticsRepository.GetNotSendApartmentsStatistics();
+                if (notSendStatistic == null)
+                {
+                    _logger.LogInformation("No statistic to send in apartments service");
+                    return;
+                }
+                var statisticResultMessage = new ApartmentsStatisticsResultMessage
+                {
+                    ApartmentsCreated = notSendStatistic.ApartmentsCreated,
+                    ApartmentsUpdated = notSendStatistic.ApartmentsUpdated,
+                    MostApartmentsOwnedByUser = notSendStatistic.MostApartmentsOwnedByUser,
+                    Year = notSendStatistic.Year.Value,
+                    Month = notSendStatistic.Month.Value,
+                    Day = notSendStatistic.Day.Value,
+                    Hour = notSendStatistic.Hour.Value
+                };
+                await _publishEndpoint.Publish(statisticResultMessage);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning("Something went wrong in SendStatisticsBackgroundJob in apartmentsService");
+                throw;
+            }
+
+
+
+
+        }
     }
 }
