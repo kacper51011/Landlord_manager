fa85f641da4002d5f0382e4cff05967293d3275d
Kacper <kacper.tylec1999@gmail.com>
2024-01-06 19:12:04 +0100

    Rooms repository gets for statistics implementation

--- Services/Rooms/Rooms.Domain/Interfaces/IRoomsRepository.cs
+++ Services/Rooms/Rooms.Domain/Interfaces/IRoomsRepository.cs
@@ -14,12 +14,12 @@ namespace Rooms.Domain.Interfaces
         public Task<Room> GetRoomByIdAndLandlordId(string landlordId, string roomId);
         public Task DeleteRoom(string roomId);
         public Task<List<Room>> GetOldestCheckedRooms();
-        public Task DeleteAllRoomsInApartment(string landlordId, string apartmentId);
+        //public Task DeleteAllRoomsInApartment(string landlordId, string apartmentId);
         public Task<Room> GetRoomById(string roomId);
 
         //gets for statistics
-        public Task<int> GetRoomsCreated(DateTime dateStart, DateTime dateEnd);
-        public Task<int> GetRoomsUpdated(DateTime dateStart, DateTime dateEnd);
+        public Task<int> GetCreatedRoomsCount(DateTime dateStart, DateTime dateEnd);
+        public Task<int> GetUpdatedRoomsCount(DateTime dateStart, DateTime dateEnd);
         public Task<int> GetMostRoomsInOneApartment(DateTime dateEnd);
         public Task<int> GetBiggestCreatedRoomSize(DateTime dateEnd);
     }
