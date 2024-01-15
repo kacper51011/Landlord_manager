2ed8ebf8beb19bfedff1cce1ec8d553d813ecba7
Kacper <kacper.tylec1999@gmail.com>
2024-01-15 20:24:13 +0100

    SendYearMessageCommand in statistics service

---
+++ Services/Statistics/Statistics.Application/Commands/MessagingTest/SendYearMessage/SendYearMessageCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Statistics.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics.Application.Commands.MessagingTest.SendYearMessage
+{
+    public record SendYearMessageCommand(CreateYearStatisticsRequestDto dto): IRequest
+    {
+    }
+}
