ea0a8832f4cd95384c38d590ac5f7c98ce0ad07e
Kacper <kacper.tylec1999@gmail.com>
2024-01-17 17:44:31 +0100

    TenantYearStatisticMessageConsumer

---
+++ Services/Tenants/Tenants.Application/Consumers/Statistics/TenantYearStatisticMessageConsumer.cs
@@ -0,0 +1,50 @@
+﻿using Contracts.StatisticsMessages;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tenants.Domain;
+using Tenants.Domain.Interfaces;
+
+namespace Tenants.Application.Consumers.Statistics
+{
+    public class TenantYearStatisticsMessageConsumer : IConsumer<StatisticYearMessage>
+    {
+        private readonly ILogger<TenantYearStatisticsMessageConsumer> _logger;
+        private readonly ITenantsStatisticsRepository _tenantsStatisticsRepository;
+        public TenantYearStatisticsMessageConsumer(ILogger<TenantYearStatisticsMessageConsumer> logger, ITenantsStatisticsRepository tenantsStatisticsRepository)
+        {
+            _logger = logger;
+            _tenantsStatisticsRepository = tenantsStatisticsRepository;
+
+        }
+        public async Task Consume(ConsumeContext<StatisticYearMessage> context)
+        {
+            try
+            {
+                var response = await _tenantsStatisticsRepository.GetTenantsAnyStatistics(context.Message.Year, null, null, null);
+                if (response != null)
+                {
+                    throw new DuplicateNameException("Statistic already created");
+                }
+                TenantsStatistics tenantsStatistic = TenantsStatistics.CreateAsYearStatisticsInformations(context.Message.Year);
+                await _tenantsStatisticsRepository.CreateOrUpdateTenantsStatistics(tenantsStatistic);
+                _logger.LogInformation($"Year tenants statistic created with Id {tenantsStatistic.TenantsStatisticsId}");
+                return;
+            }
+            catch (DuplicateNameException ex)
+            {
+                _logger.LogWarning(400, ex, ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogWarning(500, ex, "TenantYearStatisticsMessageConsumer failed");
+            }
+        }
+    }
+}
