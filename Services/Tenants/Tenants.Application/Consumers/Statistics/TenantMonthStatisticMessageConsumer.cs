a8cca60214648e59a97037c1951a94ec4ef0bb47
Kacper <kacper.tylec1999@gmail.com>
2024-01-17 17:44:55 +0100

    TenantMonthStatisticMessageConsumer

---
+++ Services/Tenants/Tenants.Application/Consumers/Statistics/TenantMonthStatisticMessageConsumer.cs
@@ -0,0 +1,50 @@
+﻿using Contracts.StatisticsMessages;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tenants.Domain;
+using Tenants.Domain.Interfaces;
+
+namespace Tenants.Application.Consumers.Statistics
+{
+    public class TenantMonthStatisticsMessageConsumer : IConsumer<StatisticMonthMessage>
+    {
+        private readonly ILogger<TenantMonthStatisticsMessageConsumer> _logger;
+        private readonly ITenantsStatisticsRepository _tenantsStatisticsRepository;
+        public TenantMonthStatisticsMessageConsumer(ILogger<TenantMonthStatisticsMessageConsumer> logger, ITenantsStatisticsRepository tenantsStatisticsRepository)
+        {
+            _logger = logger;
+            _tenantsStatisticsRepository = tenantsStatisticsRepository;
+
+        }
+        public async Task Consume(ConsumeContext<StatisticMonthMessage> context)
+        {
+            try
+            {
+                var response = await _tenantsStatisticsRepository.GetTenantsAnyStatistics(context.Message.Year, context.Message.Month, null, null);
+                if (response != null)
+                {
+                    throw new DuplicateNameException("Statistic already created");
+                }
+                TenantsStatistics tenantsStatistic = TenantsStatistics.CreateAsMonthStatisticsInformations(context.Message.Year, context.Message.Month);
+                await _tenantsStatisticsRepository.CreateOrUpdateTenantsStatistics(tenantsStatistic);
+                _logger.LogInformation($"Month tenants statistic created with Id {tenantsStatistic.TenantsStatisticsId}");
+                return;
+            }
+            catch (DuplicateNameException ex)
+            {
+                _logger.LogWarning(400, ex, ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogWarning(500, ex, "TenantMonthStatisticsMessageConsumer failed");
+            }
+        }
+    }
+}
